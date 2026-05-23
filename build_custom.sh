d6e079a07d6090cb372db8a371d82d3f9c877da6
David Rogers <rogers.dave@outlook.com>
2026-05-23 13:38:50 +0930

    make file and echo it 10

---
+++ build_custom.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+artifacts_directory="$GITHUB_WORKSPACE/artifacts"
+nuget_directory="$artifacts_directory/nuget"
+source_directory="$GITHUB_WORKSPACE/src/BusinessValidation"
+publish_directory_NET5="$source_directory/BusinessValidation/bin/Release/net5.0/publish"
+soln_path="$source_directory/BusinessValidation.sln"
+proj_path="$source_directory/BusinessValidation/BusinessValidation.csproj"
+test_proj_path="$source_directory/BusinessValidation.Tests/BusinessValidation.Tests.csproj"
+echo $nuget_directory
+echo $publish_directory_NET5
+echo $soln_path
+echo $proj_path
+echo $test_proj_path
+dotnet restore $soln_path
+dotnet publish $proj_path --no-restore --configuration Release --property:Copyright="David Rogers 2022 - 2026" --property:AssemblyVersion=${{ steps.gitversion.outputs.assemblySemVer }} --property:FileVersion=${{ steps.gitversion.outputs.majorMinorPatch }} --property:Version=${{ steps.gitversion.outputs.semVer }} --property:InformationalVersion=${{ steps.gitversion.outputs.informationalVersion }} --framework net5.0
+dotnet publish $test_proj_path --no-restore --configuration Release --property:Copyright="David Rogers 2022 - 2026" --property:AssemblyVersion=${{ steps.gitversion.outputs.assemblySemVer }} --property:FileVersion=${{ steps.gitversion.outputs.majorMinorPatch }} --property:Version=${{ steps.gitversion.outputs.semVer }} --property:InformationalVersion=${{ steps.gitversion.outputs.informationalVersion }} --framework net5.0
